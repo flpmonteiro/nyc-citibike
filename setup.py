7e7b9cc35479bb03278804d0f9a7ffc5d49ca8cc
felipe <25232105+fpmonteiro@users.noreply.github.com>
2024-04-04 13:15:08 -0300

    Initialize Dagster scaffold

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="nyc_citibike",
+    packages=find_packages(exclude=["nyc_citibike_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
